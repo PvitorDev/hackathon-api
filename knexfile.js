1d7c7c9f366171b2f9744c89b7318a49d58db178
PvitorDev <pvitor.dev@gmail.com>
2022-11-03 16:47:35 -0300

    Nao sei

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,6 @@ module.exports = {
       database: process.env.DATABASE,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      ssl: { rejectUnauthorized: false },
     },
     migrations: {
       directory: `${__dirname}/src/config/migrations`,
