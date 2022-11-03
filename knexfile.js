97a0f76b4cb2ed7d905cac1c564a4bbb968cb80b
PvitorDev <pvitor.dev@gmail.com>
2022-11-03 16:52:26 -0300

    Pfvr funciona em nome de jesus

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,8 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       database: process.env.DATABASE,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
