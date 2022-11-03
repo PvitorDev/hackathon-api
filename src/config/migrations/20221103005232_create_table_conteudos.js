9848211b499d4d4854b5a7023dd7d6b849b7a170
PvitorDev <pvitor.dev@gmail.com>
2022-11-02 22:40:11 -0300

    ok

---
+++ src/config/migrations/20221103005232_create_table_conteudos.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+  return knex.schema.createTable("usuarios", (table) => {
+    table.increments("id");
+    table.text("nome").notNullable();
+    table.text("email").unique().notNullable();
+    table.text("senha").notNullable();
+    table.text("trilha");
+    table.text("funcao");
+    table.bigint("xp").defaultTo(0);
+    table.bigint("nivel").defaultTo(0);
+    table.boolean("adm").defaultTo(false);
+    table.timestamp("criado_em").defaultTo(knex.fn.now());
+    table.timestamp("atualizado_em").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("conteudos");
+};
