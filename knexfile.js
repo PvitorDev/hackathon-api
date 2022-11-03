487dfc6cfe0b06f5001166e5ae651625096eb461
PvitorDev <pvitor.dev@gmail.com>
2022-11-03 16:24:24 -0300

    KnexFile

--- knexfile.js
+++ knexfile.js
@@ -22,9 +22,9 @@ module.exports = {
     connection: {
       user: process.env.DB_USER,
       database: process.env.DATABASE,
-      user: process.env.DB_USER,
       password: process.env.DB_PASS,
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       ssl: true,
     },
     migrations: {
