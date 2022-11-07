4ebe1a32ded1f54593efe95c7991f80f708b3fe2
PvitorDev <pvitor.dev@gmail.com>
2022-11-07 14:06:33 -0300

    Conflito com o merge resolvido

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,9 +1,5 @@
 const knexfile = require("../../../knexfile");
 
-<<<<<<< HEAD
-const knex = require("knex")(knexfile["production"]);
-=======
 const knex = require("knex")(knexfile[process.env.KNEXFILE]);
->>>>>>> dev
 
 module.exports = knex;
